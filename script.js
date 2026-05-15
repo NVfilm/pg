7adedfa3210f8ce9471feedd9a1a877d0961103e
NVfilm <dadadesigner19@gmail.com>
2026-05-15 14:04:31 +0530

    Add files via upload

--- script.js
+++
@@ -1,9 +0,0 @@
-const menuBtn = document.getElementById("menuBtn");
-
-const navMenu = document.getElementById("navMenu");
-
-menuBtn.addEventListener("click", () => {
-
-  navMenu.classList.toggle("active");
-
-});
